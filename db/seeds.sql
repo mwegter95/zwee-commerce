e0454140581cb98831445462b61bd91750655e0a
hitmaker <mwegter95@gmail.com>
2022-01-16 16:56:09 -0600

    crud routes finished for Product and Category

---
+++ db/seeds.sql
@@ -0,0 +1,8 @@
+INSERT INTO category (category_name)
+VALUES
+('Used Gear'), ('New Gear');
+
+INSERT INTO product (product_name, price, stock, category_id)
+VALUES
+('AKG Perception 220', 200, DEFAULT, 2), 
+('Pair CDJ 2000 NXS2 with mixer', 1400, DEFAULT, 1);
